957b87d3046b05560ced5353bcaa40074b11efc8
Cesar Jauregui <cesarjauregui@MacBook-Air-de-Cesar.local>
2026-05-20 13:13:09 -0600

    Configurar exportación estática para Cloudflare Pages

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
   images: {
-    remotePatterns: [
-      { protocol: "https", hostname: "drive.google.com" },
-      { protocol: "https", hostname: "lh3.googleusercontent.com" },
-      { protocol: "https", hostname: "**.googleapis.com" },
-    ],
+    unoptimized: true,
   },
 };
 
